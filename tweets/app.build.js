72f02899c6dd7e5c48815e33f3d1f4569ca1e89c
Jack Hsu <jack.hsu@gmail.com>
2012-04-11 12:26:32 -0400

    updated config

--- tweets/app.build.js
+++ tweets/app.build.js
@@ -1,14 +1,21 @@
 ({
     appDir: ".",
     baseUrl: "js",
-    dir: "build-js",
+    dir: "../tweets-build",
     name: "index",
-    out: "built.js",
     paths: {
-        jquery: "empty:",
-        underscore: "empty:",
-        backbone: "empty:",
-        text: "empty:",
-        synapse: "empty:"
+        handlebars: 'libs/handlebars/handlebars',
+        jquery: 'libs/jquery/jquery-1.7.1.min',
+        underscore: 'libs/underscore/underscore-min',
+        backbone: 'libs/backbone/backbone',
+        localStorage: 'libs/backbone/localstorage',
+        moment: 'libs/moment/moment',
+        text: 'libs/require/text',
+        synapse: 'libs/synapse/synapse',
+        'synapse/core': 'libs/synapse/synapse',
+        'synapse/hooks/object': 'libs/synapse/hooks/object',
+        'synapse/hooks/jquery': 'libs/synapse/hooks/jquery',
+        'synapse/hooks/backbone-model': 'libs/synapse/hooks/backbone-model',
+        'synapse/hooks/backbone-view': 'libs/synapse/hooks/backbone-view'
     }
 })
