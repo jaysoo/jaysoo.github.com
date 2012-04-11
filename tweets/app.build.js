a5ec8509beb9d165d13186427b5462f4595bc244
Jack Hsu <jack.hsu@gmail.com>
2012-04-11 10:44:18 -0400

    Added optimizer

---
+++ tweets/app.build.js
@@ -0,0 +1,14 @@
+({
+    appDir: ".",
+    baseUrl: "js",
+    dir: "build-js",
+    name: "index",
+    out: "built.js",
+    paths: {
+        jquery: "empty:",
+        underscore: "empty:",
+        backbone: "empty:",
+        text: "empty:",
+        synapse: "empty:"
+    }
+})
